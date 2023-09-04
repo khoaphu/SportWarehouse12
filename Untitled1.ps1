9c8ada00f4766e957ca87a5785af31efd960ea4f
khoaphu <142789057+khoaphu@users.noreply.github.com>
2023-09-04 19:55:35 +1000

    Add files via upload

---
+++ Untitled1.ps1
@@ -0,0 +1,28 @@
+﻿$url = "c:\temp\sample1.json"
+
+$response = Invoke-WebRequest -Uri $url -UseBasicParsing
+
+ 
+
+$jsons = $response | ConvertFrom-Json
+
+$jsons | FT
+
+#write-host $response
+
+foreach ($json in $jsons){
+
+# write-host "$($user.links) has the email: $($user.name)"
+
+# Out-File tables.txt
+
+}
+# $table = $users.tables
+
+write-host $jsons.tables 
+
+foreach ($table in $tables){
+
+
+
+}
